7c04861de4dbb5ecbd97551a9e0efd6c90998eaf
Stefan Heimberg <kontakt@stefanheimberg.ch>
2019-07-21 22:33:58 +0200

    aufgabe 3 meine lösung

--- basics-assignment-3/src/app/app.component.ts
+++ basics-assignment-3/src/app/app.component.ts
@@ -3,7 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
+  displayContent = false;
+  clicks = [];
+  
+  onDisplayContentClick(event: Event) {
+    this.displayContent = true;
+    this.clicks.push(event.timeStamp);
+  }
+
+  getColor(index: number) {
+    return (index+1 >= 5) ? 'blue' : '';
+  }
+
 }
