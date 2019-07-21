da184e4a37a3d33c61917c0160ffb710eef110f5
Stefan Heimberg <kontakt@stefanheimberg.ch>
2019-07-21 23:50:00 +0200

    Kapitel 46. "Creating the Components"

--- course-project-app/src/app/app.module.ts
+++ course-project-app/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RecipesModule,
+    ShoppingListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ course-project-app/src/app/recipes/recipe-list/recipe-list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RecipeListComponent } from './recipe-list.component';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+
+@NgModule({
+    declarations: [
+        RecipeItemComponent
+    ],
+    imports: [
+        CommonModule
+    ]
+})
+export class RecipeListModule { }
