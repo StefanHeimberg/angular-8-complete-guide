e88911d37d0f9f78d9af39d57353ace1a5197f42
Stefan Heimberg <kontakt@stefanheimberg.ch>
2019-07-23 23:01:19 +0200

    Kapitel 68. Assigning an Alias to Custom Events

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -10,7 +10,7 @@ export class CockpitComponent implements OnInit {
   @Output()
   serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   
-  @Output()
+  @Output('bpCreated')
   blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
 
   newServerName = '';
